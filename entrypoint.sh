64de24a08e7b0c4899438286c6f8ac37ba484d90
Tara S <tara.schoenherr@gmail.com>
2024-02-24 16:57:04 -0700

    update deletion logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,8 +44,8 @@ ls -la "$CLONE_DIRECTORY"
 echo
 echo "##### Delete Existing Directory Contents in Detination Directory #####"
 echo "$CLONE_DIRECTORY/$SOURCE_FILES"
-if [ -d "$CLONE_DIRECTORY/$SOURCE_FILES" ]; then
-  rm -rf "$CLONE_DIRECTORY/$SOURCE_FILES"
+if [ -d "$DESTINATION_DIRECTORY" ]; then
+  rm -rf "$DESTINATION_DIRECTORY"
 fi
 
 echo
